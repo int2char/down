b494f4b4df09626aca2bb250d2ea79dbf0cdba4e
int2char <zhangshaoqianse@163.com>
2018-04-15 22:30:26 +0800

    kkk

--- include/NewGA.h
+++ include/NewGA.h
@@ -79,19 +79,25 @@ class NewGA
 			cout<<"ops"<<endl;
 			vector<double>capacity(EDge,100);
 			vector<pair<string,double>> rdata;
-			vector<double>u(EDge,1);
+			vector<double>u(EDge,0.1);
+			vector<double>f(EDge,0);
 			vector<vector<pair<int,int>>>epath(EDge,vector<pair<int,int>>());
 			map<pair<int,int>,vector<int>>pathe;
 			vector<vector<double>>x(ser.size(),vector<double>());
 			vector<double>y(ser.size(),0);
+			vector<vector<double>>overtime(ser.size(),std::vector<double>());
+			map<pair<int,int>,double>wsigu;
+			vector<double> tsum(ser.size(),-1e10);
 			double alpha=4;
 			double q=0.9;
 			cout<<"asd"<<endl;
+			_PathSets[111]=_PathSets[0];
 			for(int i=0;i<ser.size();i++)
 			{
 				double as=0;
-				int num=min(_PathSets[i].num,10);
+				int num=min(_PathSets[i].num,5);
 				//cout<<_PathSets[i].num<<endl;
+				if(num<2)cout<<"axc: "<<i<<endl;
 				for(int j=0;j<num;j++)
 					{
 						if(_PathSets[i].Pathset[j][0]>-1)
@@ -106,6 +112,7 @@ class NewGA
 								pathe[make_pair(i,j)].push_back(_PathSets[i].Pathset[j][k]);
 								k++;
 							}
+							overtime[i].push_back(1);
 					}
 				y[i]=as;
 			}
@@ -117,89 +124,127 @@ class NewGA
 			double bestv=-INT_MAX/2;
 			int count=0;
 			double tt=1;
-			for(int t=0;t<10000000;t++)
+			for(int t=0;t<10000;t++)
 			{
-				if(t>1000)
-					{if(t<10)
-						tt++;
 
-					}
+				cout<<"before init "<<endl;
 				vector<double> sigxt(ser.size(),0);
 				map<pair<int,int>,double>sigu;
+				map<pair<int,int>,double>sigf;
 				vector<double>esigxt(EDge,0);
 				vector<double>tu(EDge,1);
 				vector<double>asigxt(ser.size(),0);
 				vector<double>ty(ser.size(),0);
 				vector<vector<double>>tx(ser.size(),vector<double>());
+				vector<double>flag(ser.size(),0);
+				cout<<"init sucess"<<endl;
 				double objective=0;
 				for(int i=0;i<EDge;i++)
 					for(int j=0;j<epath[i].size();j++)
 						sigu[epath[i][j]]+=u[i];
 				for(int i=0;i<ser.size();i++)
+					for(int j=0;j<x[i].size();j++)
+						for(int k=0;k<pathe[make_pair(i,j)].size();k++)
+							esigxt[pathe[make_pair(i,j)][k]]+=x[i][j];
+				cout<<"before u"<<endl;
+				for(int i=0;i<EDge;i++)
 					{
-						double max=0;
+							
+							tu[i]=u[i]+(0.05)*u[i]*(esigxt[i]-capacity[i])/capacity[i];
+							if(esigxt[i]-capacity[i]>0)
+								for(int k=0;k<epath[i].size();k++)
+									{
+										flag[epath[i][k].first]=1;
+									}
+					}
+				cout<<"after u"<<endl;
+				for(int i=0;i<ser.size();i++)
+				{
+					for(int j=0;j<x[i].size();j++)
+						asigxt[i]+=pow(x[i][j],q);
+					if(flag[i]>0.5&&asigxt[i]-pow(y[i],q)<0)
+						ty[i]=y[i];
+					else
+						ty[i]=y[i]+((1-q)/(2*(alpha+q-1)))*y[i]*(asigxt[i]-pow(y[i],q))/pow((y[i]),q);
+				}
+				cout<<"agter y"<<endl;
+				for(int i=0;i<ser.size();i++)
+					{
+						double min=1e20;
 						int id=-1;
 						double toto=0;
+						double sum=0;
 						for(int j=0;j<x[i].size();j++)
 						{
-							double data=pow(pow(y[i],-alpha)/sigu[make_pair(i,j)],1/(1-q));
-							if(data>max){
-								max=data;
+							double data=sigu[make_pair(i,j)];
+							sum+=sigu[make_pair(i,j)];
+							if(data<min){
+								min=data;
 								id=j;
 							}
 							toto+=data;
 							tx[i].push_back(data);
 						}
 						for(int j=0;j<tx[i].size();j++)
-						{
-							if(tx[i][j]<=max)tx[i][j]=0;
-						}
-						tx[i][id]=max;
+							tx[i][j]=0;
+						//cout<<sum<<" "<<tsum[i]<<endl;
+						if(x[i][id]>0&&flag[i]<0.5)
+							sum=max(sum,tsum[i]);
+						tx[i][id]=pow(pow(ty[i],-alpha)/sum,1/(1-q))*ty[i];
+						tsum[i]=sum;
+						if(tsum[i]==0)cout<<"erro!!!"<<endl;
 					}
+				cout<<"after tx"<<endl;
+				/*int overflow=0;
+				vector<double> ca=capacity;
+				double toto=0;
 				for(int i=0;i<ser.size();i++)
+				{
 					for(int j=0;j<x[i].size();j++)
-						for(int k=0;k<pathe[make_pair(i,j)].size();k++)
-							esigxt[pathe[make_pair(i,j)][k]]+=x[i][j];
+						{
+							if(x[i][j]>0)
+							toto+=x[i][j];
+							for(int k=0;k<pathe[make_pair(i,j)].size();k++)
+								ca[pathe[make_pair(i,j)][k]]-=x[i][j];
+						}
+				}
+				int gg=0;
 				for(int i=0;i<EDge;i++)
-					tu[i]=u[i]+(1/tt)*((1-q)/2)*u[i]*((esigxt[i]-capacity[i])/capacity[i]);
-				for(int i=0;i<ser.size();i++)
-					{
-						for(int j=0;j<x[i].size();j++)
-							asigxt[i]+=pow(x[i][j],q);
-						ty[i]=y[i]+(1/tt)*((1-q)/(2*(alpha+q-1)))*y[i]*(asigxt[i]-pow(y[i],q))/(pow(y[i],q));
-					}
+					if(ca[i]<-1)
+						{
+							gg=1;
+							cout<<"&&&&&&&&&&&&&&&&& "<<i<<endl;
+							for(int k=0;k<epath[i].size();k++)
+								{
+									int a=epath[i][k].first;
+									int b=epath[i][k].second;
+									if(x[a][b]>0)
+										cout<<"************ "<<a<<" "<<y[a]<<" "<<ty[a]<<" "<<x[a][b]<<" "<<wsigu[epath[i][k]]<<" "<<sigu[epath[i][k]]<<" "<<tx[a][b]<<endl;
+								}
+							//cout<<i<<" "<<ca[i]<<endl;
+							overflow++;
+						}
+				*/
 				x=tx;
 				y=ty;
 				u=tu;
-				for(int i=0;i<ser.size();i++)
-					objective+=-pow(y[i],-3)/3;
-				count++;
-				if(objective>bestv)
+				wsigu=sigu;
+				//for(int i=0;i<ser.size();i++)
+					//objective+=-pow(asigxt[i],-3)/3;
+				//count++;
+				//if(gg>0)cout<<t<<endl;
+				//if(objective>bestv)
 				{
-					count=0;
-					bestv=objective;
+					//count=0;
+					//bestv=objective;
 				}
-				if(count>100000)break;
+				//if(count>200)break;
+				//cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;*/
+				//cout<<"obj is: "<<overflow<<" "<<toto<<" "<<objective<<endl;
 				cout<<"obj is: "<<objective<<endl;
 			}
 			int toto=0;
-			for(int i=0;i<ser.size();i++)
-				{
-					for(int j=0;j<x[i].size();j++)
-						{
-							toto+=x[i][j];
-							if(x[i][j]>=10)
-							cout<<x[i][j]<<" ";
-							else
-								cout<<0<<" ";
-							for(int k=0;k<pathe[make_pair(i,j)].size();k++)
-								capacity[pathe[make_pair(i,j)][k]]-=x[i][j];
-						}
-					cout<<endl;
-				}
-			for(int i=0;i<EDge;i++)
-				if(capacity[i]<-1)
-					cout<<"overflow "<<endl;
+			cout<<"bestv is "<<bestv<<endl;
 			cout<<"toto is "<<toto<<endl;
 			return rdata;
 		};
--- src/grab.sh
+++ src/grab.sh
@@ -18,7 +18,7 @@ do
       task=$[$power*$node]
       biao=$type
       rm -f *.out
-      nvcc -O3 -std=c++11 *.cpp *.cu --gpu-architecture=compute_35 --gpu-code=sm_35 -I ../include -I ../cplex_include -L ../lib -lconcert -lcplex -lilocplex -lm -lpthread -DIL_STD -DNODE=$node -DEDge=$edge -DTask=$task -DTYPE="\"${graphtype[$biao]}\"" -DINPUTFILE=\"$path\" -DGANOEX=1 #2>>./data/complie.txt
+      nvcc  -std=c++11 *.cpp *.cu --gpu-architecture=compute_35 --gpu-code=sm_35 -I ../include -I ../cplex_include -L ../lib -lconcert -lcplex -lilocplex -lm -lpthread -DIL_STD -DNODE=$node -DEDge=$edge -DTask=$task -DTYPE="\"${graphtype[$biao]}\"" -DINPUTFILE=\"$path\" -DGANOEX=1 #2>>./data/complie.txt
     # CUDA_VISIBLE_DEVICES=0 ./a.out G T 1>>./data/runinfo.txt 2>>./data/err.txt
        if [[ $power -eq 6 ]];then
 	for capacity in ${capacitys[@]}
